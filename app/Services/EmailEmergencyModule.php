2a53744b6fcd122e06ab576abb5bf612bfccdeaf
Cezar Dias <cezardias@gmail.com>
2026-03-27 19:41:57 -0300

    fix: modern mailable syntax for all email types

--- app/Services/EmailEmergencyModule.php
+++ app/Services/EmailEmergencyModule.php
@@ -30,60 +30,45 @@ class EmailEmergencyModule
         try {
             self::setSMTP();
             $email_destino = strtolower(trim($email_destino));
-            Log::info("DEBUG-MAIL: Enviando CONFIRMACAO para $email_destino");
+            Log::info("EMERGENCY-MAIL: Enviando CONFIRMACAO para $email_destino");
             
-            Mail::send('emails.confirmacao_agendamento', ['params' => $params], function ($m) use ($email_destino) {
-                $m->from(config('mail.from.address'), config('app.name', 'Medical Place'));
-                $m->to($email_destino)->subject('Agendamento Confirmado - Medical Place');
-            });
+            \Mail::to($email_destino)->send(new \App\Mail\ConfirmacaoAgendamento($params));
             
             return true;
         } catch (\Exception $e) {
-            Log::error("DEBUG-MAIL: ERRO Confirmação: " . $e->getMessage());
+            Log::error("EMERGENCY-MAIL: ERRO Confirmação: " . $e->getMessage());
             return false;
         }
     }
 
-    /**
-     * Envia e-mail de cancelamento (Síncrono)
-     */
     public static function enviarCancelamento($params, $email_destino)
     {
         try {
             self::setSMTP();
             $email_destino = strtolower(trim($email_destino));
-            Log::info("DEBUG-MAIL: Enviando Cancelamento para $email_destino");
+            Log::info("EMERGENCY-MAIL: Enviando CANCELAMENTO para $email_destino");
             
-            Mail::send('emails.cancelamento_agendamento', ['params' => $params], function ($m) use ($email_destino) {
-                $m->from(config('mail.from.address'), config('app.name', 'Medical Place'));
-                $m->to($email_destino)->subject('Agendamento Cancelado - Medical Place');
-            });
+            \Mail::to($email_destino)->send(new \App\Mail\CancelamentoAgendamento($params));
             
             return true;
         } catch (\Exception $e) {
-            Log::error("DEBUG-MAIL: ERRO Cancelamento: " . $e->getMessage());
+            Log::error("EMERGENCY-MAIL: ERRO Cancelamento: " . $e->getMessage());
             return false;
         }
     }
 
-    /**
-     * Envia e-mail de boas-vindas (Síncrono)
-     */
     public static function enviarBoasVindas($params, $email_destino)
     {
         try {
             self::setSMTP();
             $email_destino = strtolower(trim($email_destino));
-            Log::info("DEBUG-MAIL: Enviando Boas-vindas para $email_destino");
+            Log::info("EMERGENCY-MAIL: Enviando BOAS-VINDAS para $email_destino");
             
-            Mail::send('emails.boas_vindas_medico', ['params' => $params], function ($m) use ($email_destino) {
-                $m->from(config('mail.from.address'), config('app.name', 'Medical Place'));
-                $m->to($email_destino)->subject('Bem-vindo a Medical Place!');
-            });
+            \Mail::to($email_destino)->send(new \App\Mail\BoasVindasMedico($params));
             
             return true;
         } catch (\Exception $e) {
-            Log::error("DEBUG-MAIL: ERRO Boas-vindas: " . $e->getMessage());
+            Log::error("EMERGENCY-MAIL: ERRO Boas-vindas: " . $e->getMessage());
             return false;
         }
     }
